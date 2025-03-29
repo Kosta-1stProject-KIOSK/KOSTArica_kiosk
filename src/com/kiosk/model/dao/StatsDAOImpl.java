832d59f11d6566a5f852a0ab3feeb2e8d59cfd6b
HanbinKim <hbkimion@gmail.com>
2025-03-29 16:05:38 +0900

    DAO 인터페이스 1차 작업 끝. 검토해주세요

--- src/com/kiosk/model/dao/StatsDAOImpl.java
+++ src/com/kiosk/model/dao/StatsDAOImpl.java
@@ -1,5 +1,57 @@
 package com.kiosk.model.dao;
 
+import java.util.List;
+
+import com.kiosk.model.dto.StatsDTO;
+
 public class StatsDAOImpl implements StatsDAO {
+	
+	/**
+	 * 기간에 따른 매출 통계 조회
+	 * @param periodType (기간 일간, 주간, 월간)
+	 * @return 매출 통계 List
+	 */
+	@Override
+	public List<StatsDTO> getSalesByMenu(String periodType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	/**
+     * 가장 많이 판매된 상위 N개의 메뉴 조회
+     * @param limit (상위 개수 제한)
+     * @return 인기 메뉴 리스트
+     */
+	@Override
+	public List<StatsDTO> getTopMenus(int limit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	/**
+     * 전체 총 매출 금액 반환
+     * @return 총 매출 금액
+     */
+	@Override
+	public int getTotalSales() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	/**
+     * 전체 주문 수를 반환합니다.
+     * @return 총 주문 수
+     */
+	@Override
+	public int getTotalOrders() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
